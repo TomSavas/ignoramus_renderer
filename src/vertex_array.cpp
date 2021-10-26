cd2ce2932cf618870bca67816cd0ebdc726742dc
TomSavas <tomas.savickas@protonmail.com>
2021-10-26 23:51:57 +0000

    Remove redundant OpenGL error checks

--- src/vertex_array.cpp
+++ src/vertex_array.cpp
@@ -5,41 +5,17 @@
 
 VertexArray::VertexArray()
 {
-    GLenum err;
-    while((err = glGetError()) != GL_NO_ERROR)
-    {
-        printf("[pre gl gen vertex arrays] OpenGL error: %04x\n", err);
-        assert(false);
-    }
-
     glGenVertexArrays(1, &id);
-
-    while((err = glGetError()) != GL_NO_ERROR)
-        printf("[post gl gen vertex arrays] OpenGL error: %04x\n", err);
-
     Bind();
-    while((err = glGetError()) != GL_NO_ERROR)
-        printf("[post bind] OpenGL error: %04x\n", err);
 }
 
 VertexArray::VertexArray(VertexBuffer vertexBuffer, IndexBuffer indexBuffer,
         BufferLayout bufferLayout) 
     : VertexArray()
 {
-    GLenum err;
-
     AddVertexBuffer(vertexBuffer);
-    while((err = glGetError()) != GL_NO_ERROR)
-        printf("[post add vertex buffer] OpenGL error: %04x\n", err);
-
     SetIndexBuffer(indexBuffer);
-    while((err = glGetError()) != GL_NO_ERROR)
-        printf("[post set index buffer] OpenGL error: %04x\n", err);
-
     SetBufferLayout(bufferLayout);
-    while((err = glGetError()) != GL_NO_ERROR)
-        printf("[post set buffer layout] OpenGL error: %04x\n", err);
-
 }
 
 void VertexArray::Bind() const 
